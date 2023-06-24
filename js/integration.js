e8f3bc87186b8ec1060936f7226caf2d9408c8c0
Amin <aminmajid.dev@gmail.com>
2023-06-25 03:48:03 +0500

    improved codebase

--- js/integration.js
+++ js/integration.js
@@ -1,116 +1,124 @@
+function rcs__createFormElement(elementType, className, textContent, inputType, placeholder, id) {
+    const element = document.createElement(elementType);
+    element.className = className;
+    element.textContent = textContent;
+  
+    if (elementType === 'input') {
+      element.type = inputType;
+      element.placeholder = placeholder;
+      element.id = id;
+    } else if (elementType === 'textarea') {
+      element.id = id;
+    } else if (elementType === 'button') {
+      element.type = 'submit';
+    }
+    element.required = true;
+  
+    return element;
+  }
+  
+  // Store classes and IDs in variables
+  const rcs__formClass = 'rcs-form';
+  const rcs__formRowClass = 'rcs-formRow';
+  const rcs__formColClass = 'rcs-formCol';
+  const rcs__inputLabelClass = 'rcs-inputLabel';
+  const rcs__inputTextClass = 'rcs-inputText';
+  const rcs__textRightClass = 'rcs-textRight';
+  const rcs__btnClass = 'rcs-btn';
+  const rcs__btnGreenClass = 'rcs-btnGreen';
+  const rcs__formTargetDivId = 'resourcex-customer-support-form';
+  
+  // Create the form element
+  const rcs__form = rcs__createFormElement('form', rcs__formClass, '');
+  
+  // Create the first form row
+  const rcs__formRow1 = rcs__createFormElement('div', rcs__formRowClass, '');
+  
+  const rcs__nameCol = rcs__createFormElement('div', rcs__formColClass, '');
+  const rcs__nameInputLabel = rcs__createFormElement('div', rcs__inputLabelClass, 'Your Name');
+  const rcs__nameInput = rcs__createFormElement('input', rcs__inputTextClass, '', 'text', 'John Doe', 'name');
+  
+  rcs__nameCol.appendChild(rcs__nameInputLabel);
+  rcs__nameCol.appendChild(rcs__nameInput);
+  rcs__formRow1.appendChild(rcs__nameCol);
+  
+  const rcs__emailCol = rcs__createFormElement('div', rcs__formColClass, '');
+  const rcs__emailInputLabel = rcs__createFormElement('div', rcs__inputLabelClass, 'Your Email');
+  const rcs__emailInput = rcs__createFormElement('input', rcs__inputTextClass, '', 'email', 'johndoe@example.com', 'email');
+  
+  rcs__emailCol.appendChild(rcs__emailInputLabel);
+  rcs__emailCol.appendChild(rcs__emailInput);
+  rcs__formRow1.appendChild(rcs__emailCol);
+  
+  rcs__form.appendChild(rcs__formRow1);
+  
+  // Create the second form row
+  const rcs__formRow2 = rcs__createFormElement('div', rcs__formRowClass, '');
+  
+  const rcs__modelCol = rcs__createFormElement('div', rcs__formColClass, '');
+  const rcs__modelInputLabel = rcs__createFormElement('div', rcs__inputLabelClass, 'Model #');
+  const rcs__modelInput = rcs__createFormElement('input', rcs__inputTextClass, '', 'text', 'MRM-3242', 'model_id');
+  
+  rcs__modelCol.appendChild(rcs__modelInputLabel);
+  rcs__modelCol.appendChild(rcs__modelInput);
+  rcs__formRow2.appendChild(rcs__modelCol);
+  
+  const rcs__partCol = rcs__createFormElement('div', rcs__formColClass, '');
+  const rcs__partInputLabel = rcs__createFormElement('div', rcs__inputLabelClass, 'Part #');
+  const rcs__partInput = rcs__createFormElement('input', rcs__inputTextClass, '', 'text', 'optional', 'part_id');
+  
+  rcs__partCol.appendChild(rcs__partInputLabel);
+  rcs__partCol.appendChild(rcs__partInput);
+  rcs__formRow2.appendChild(rcs__partCol);
+  
+  rcs__form.appendChild(rcs__formRow2);
+  
+  // Create the third form row
+  const rcs__formRow3 = rcs__createFormElement('div', rcs__formRowClass, '');
+  
+  const rcs__questionCol = rcs__createFormElement('div', rcs__formColClass, '');
+  const rcs__questionInputLabel = rcs__createFormElement('div', rcs__inputLabelClass, 'Question');
+  const rcs__questionTextarea = rcs__createFormElement('textarea', '', '', '', '', 'question_text');
+  
+  rcs__questionCol.appendChild(rcs__questionInputLabel);
+  rcs__questionCol.appendChild(rcs__questionTextarea);
+  rcs__formRow3.appendChild(rcs__questionCol);
+  
+  rcs__form.appendChild(rcs__formRow3);
+  
+  // Create the fourth form row
+  const rcs__formRow4 = rcs__createFormElement('div', rcs__formRowClass, '');
+  
+  const rcs__submitCol = rcs__createFormElement('div', `${rcs__formColClass} ${rcs__textRightClass}`, '');
+  const rcs__submitButton = rcs__createFormElement('button', `${rcs__btnClass} ${rcs__btnGreenClass}`, 'Submit');
+  
+  rcs__submitCol.appendChild(rcs__submitButton);
+  rcs__formRow4.appendChild(rcs__submitCol);
+  
+  rcs__form.appendChild(rcs__formRow4);
+  
+  // Append the form to the target div
+  const rcs__targetDiv = document.getElementById(rcs__formTargetDivId);
+  rcs__targetDiv.appendChild(rcs__form);
+  
+  rcs__form.addEventListener('submit', (e) => {
+    e.preventDefault();
 
-// Create the form element
-const form = document.createElement('form');
-form.className = 'rcs-form';
-form.setAttribute('onSubmit', 'submitTicket()');
+    const rcs__formData = {
+      name: rcs__getValue('name'),
+      email: rcs__getValue('email'),
+      model_id: rcs__getValue('model_id'),
+      part_id: rcs__getValue('part_id'),
+      question_text: rcs__getValue('question_text'),
+      site_product_id: rcs__targetDiv.dataset.productId,
+    };
 
-// Create the first form row
-const formRow1 = createFormRow();
-const nameCol = createFormCol();
-const nameInputLabel = document.createElement('div');
-nameInputLabel.className = 'rcs-inputLabel';
-nameInputLabel.textContent = 'Your Name';
-const nameInput = document.createElement('input');
-nameInput.type = 'text';
-nameInput.className = 'rcs-inputText';
-nameInput.placeholder = 'John Doe';
-nameInput.id = 'name';
-
-nameCol.appendChild(nameInputLabel);
-nameCol.appendChild(nameInput);
-formRow1.appendChild(nameCol);
-
-const emailCol = createFormCol();
-const emailInputLabel = document.createElement('div');
-emailInputLabel.className = 'rcs-inputLabel';
-emailInputLabel.textContent = 'Your Email';
-const emailInput = document.createElement('input');
-emailInput.type = 'email';
-emailInput.className = 'rcs-inputText';
-emailInput.placeholder = 'johndoe@example.com';
-emailInput.id = 'email';
-
-emailCol.appendChild(emailInputLabel);
-emailCol.appendChild(emailInput);
-formRow1.appendChild(emailCol);
-
-form.appendChild(formRow1);
-
-// Create the second form row
-const formRow2 = createFormRow();
-const modelCol = createFormCol();
-const modelInputLabel = document.createElement('div');
-modelInputLabel.className = 'rcs-inputLabel';
-modelInputLabel.textContent = 'Model #';
-const modelInput = document.createElement('input');
-modelInput.type = 'text';
-modelInput.className = 'rcs-inputText';
-modelInput.placeholder = 'MRM-3242';
-modelInput.id = 'model_id';
-
-modelCol.appendChild(modelInputLabel);
-modelCol.appendChild(modelInput);
-formRow2.appendChild(modelCol);
-
-const partCol = createFormCol();
-const partInputLabel = document.createElement('div');
-partInputLabel.className = 'rcs-inputLabel';
-partInputLabel.textContent = 'Part #';
-const partInput = document.createElement('input');
-partInput.type = 'text';
-partInput.className = 'rcs-InputText';
-partInput.placeholder = 'optional';
-partInput.id = 'part_id';
-
-partCol.appendChild(partInputLabel);
-partCol.appendChild(partInput);
-formRow2.appendChild(partCol);
-
-form.appendChild(formRow2);
-
-// Create the third form row
-const formRow3 = createFormRow();
-const questionCol = createFormCol();
-const questionInputLabel = document.createElement('div');
-questionInputLabel.className = 'rcs-inputLabel';
-questionInputLabel.textContent = 'Question';
-const questionTextarea = document.createElement('textarea');
-questionTextarea.id = 'question_text';
-
-questionCol.appendChild(questionInputLabel);
-questionCol.appendChild(questionTextarea);
-formRow3.appendChild(questionCol);
-
-form.appendChild(formRow3);
-
-// Create the fourth form row
-const formRow4 = createFormRow();
-const submitCol = createFormCol();
-submitCol.className += ' rcs-textRight';
-const submitButton = document.createElement('button');
-submitButton.type = 'submit';
-submitButton.className = 'rcs-btn rcs-btnGreen';
-submitButton.textContent = 'Submit';
-
-submitCol.appendChild(submitButton);
-formRow4.appendChild(submitCol);
-
-form.appendChild(formRow4);
-
-// Append the form to the target div
-const targetDiv = document.getElementById('resourcex-customer-support-form');
-targetDiv.appendChild(form);
-
-// Function to create a form row
-function createFormRow() {
-    const formRow = document.createElement('div');
-    formRow.className = 'rcs-formRow';
-    return formRow;
-}
-
-// Function to create a form column
-function createFormCol() {
-    const formCol = document.createElement('div');
-    formCol.className = 'rcs-formCol';
-    return formCol;
-}
+    console.log(rcs__formData);
+  
+  });
+  
+  function rcs__getValue(elementId) {
+    const element = document.getElementById(elementId);
+    return element.value;
+  }
+  
